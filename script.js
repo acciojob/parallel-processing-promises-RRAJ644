6e3737274b3d6626161e0b0cf579606d9e437578
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-12-03 17:08:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+const downloadImage = (image) => {
+	return new Promise((resolve, reject) => {
+		const img = new Image()
+		img.src=image.url
+
+		img.onload = () => resolve(img)
+		img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
+	})
+}
+
+btn.addEventListener('click', (e)=> {
+	e.preventDefault()
+	btn.disabled = true;
+
+	Promise.all(images.map(downloadImg)).then((imgElements) => {
+		output.innerHTML = "";
+		imgElements.forEach((img) => {
+			output.appendChild(img);
+		});
+	}).catch((error) => {
+		output.innerHTML = `<p style="color: red;">${error}</p>`;
+	}).finally (()=> {
+		btn.disabled = false;
+	})
+})
